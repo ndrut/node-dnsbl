7828dcfd45160a9223f827b060a5343e5fbe4968
Jawsomator <atynefield@gmail.com>
2013-03-09 03:20:02 -0500

    Reorganize and adjust formatting now we're wrapped by a caching mechanism, I also went back to using the node default DNS commands.

--- rbl-server.js
+++ rbl-server.js
@@ -30,7 +30,50 @@ server.on('request', function (request, response) {
             }
         }
         else {
-            response.send();
+            ip = request.question[0].name.replace('rbl.iheardyouliek.com', '');
+            async.each(db, function (rbl, callback) {
+                dns.resolve(ip + rbl.dns, function (err, domain) {
+                    if(err) {
+                        callback(null);
+                    }
+                    else {
+                        response.answer.push(nDNS.A({
+                            name: request.question[0].name,
+                            address: '127.0.0.2',
+                            ttl: 300,
+                        }));
+                        response.additional.push(nDNS.TXT({
+                        name: request.question[0].name,
+                        data: rbl.dns,
+                        ttl: 300,
+                        }));
+                        callback(null);
+                    }
+                });
+            },
+            function (err) {
+                if(response.answer.length < 1) {
+                    response.header.rcode = nDNS.consts.NAME_TO_RCODE.NOTFOUND;
+                    response.send();
+                    res = {};
+                    res.answer = response.answer;
+                    res.additional = response.additional;
+                    redisClient.set(request.question[0].name, JSON.stringify(res), function (err, data) {
+                        if (err) { log.error('REDIS: ' + err); }
+                        redisClient.expire(request.question[0].name, 1800);
+                    });
+                }
+                else {
+                    response.send();
+                    res = {};
+                    res.answer = response.answer;
+                    res.additional = response.additional;
+                    redisClient.set(request.question[0].name, JSON.stringify(res), function (err, data) {
+                        if (err) { log.error('REDIS: ' + err); }
+                        redisClient.expire(request.question[0].name, 1800);
+                    });
+                }
+            });
         }
     });
     log.info(request.address.address + '\t' + request.question[0].name);
